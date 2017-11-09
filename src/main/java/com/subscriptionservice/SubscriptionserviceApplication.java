12bf0899334d71d46330284b2a529e544959f7ca
sagarnair16 <sagar.nair56@gmail.com>
2017-11-09 14:24:13 -0600

    Folder structure and jquery and bootstrapdependencies

---
+++ src/main/java/com/subscriptionservice/SubscriptionserviceApplication.java
@@ -0,0 +1,12 @@
+package com.subscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionserviceApplication.class, args);
+	}
+}
